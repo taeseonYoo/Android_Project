ec58218358008e0059d1bde39bdb610093623fe4
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2023-11-11 01:17:53 +0900

    Use firestore for chatting messages
    Feat: 채팅 DB를 구축함
    
    채팅 DB를 구축하였음
    식별에 대한 값 설정은 제대로 하지 못하였음
    채팅방 목록과 함께 테스트를 진행 해서 완성 해야 할 것이다.

--- Transaction_Project/app/src/main/java/com/example/transaction_project/chat/ChatAdapter.kt
+++ Transaction_Project/app/src/main/java/com/example/transaction_project/chat/ChatAdapter.kt
@@ -3,72 +3,78 @@ package com.example.transaction_project.chat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.transaction_project.R
 import java.lang.IllegalArgumentException
 
 
-//메세지를 DB로부터 가져 올 어댑터가 추가로 필요
-//현재는 ID로 식별하여 리싸이클러뷰에 표시되도록 하였음
-
+//처음 채팅을 시작하는 경우에 대한 해결 필요
 class ChatAdapter(val chatList : ArrayList<ChatListItem>):
-    RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
-
+    RecyclerView.Adapter<ChatAdapter.ChatViewHolder<*>>() {
     companion object{
         private const val SEND_CHAT = 0
         private const val RECIEVE_CHAT = 1
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder<*> {
         return when(viewType){
             SEND_CHAT->{ //보내는 메세지 일때는 R.layout.send_chat
                 val view = LayoutInflater.from(parent.context).inflate(R.layout.send_chat,parent,false)
-                ChatViewHolder(view)
+                SendViewHolder(view)
             }
             RECIEVE_CHAT->{ //받는 메세지 일때는 R.layout.receive_chat
                 val view = LayoutInflater.from(parent.context).inflate(R.layout.receive_chat,parent,false)
-                ChatViewHolder(view)
+                ReceiveViewHolder(view)
             }
             else-> throw IllegalArgumentException("faild")
         }
     }
 
-    inner class ChatViewHolder(chatView : View):RecyclerView.ViewHolder(chatView){
-
-
+    override fun onBindViewHolder(holder: ChatViewHolder<*>, position: Int) {
+        val message = chatList[position]
+        when(holder){
+            is SendViewHolder -> holder.bind(message)
+            is ReceiveViewHolder -> holder.bind(message)
+        }
     }
-    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
 
-    }
-
+    //메세지 수신, 발신을 확인함
     override fun getItemViewType(position: Int): Int {
-        //ChatListItem이라는 data 클래스에 식별 할 만한 것이 userID밖에 없어서 테스트
-        val nick = chatList[position].userId
 
-        if(nick=="1"){
+        val check = chatList[position].userId
+
+        if(check=="2"){ //userId가 2일때 보내는 챗
             return SEND_CHAT
         }
         return RECIEVE_CHAT
-
     }
     override fun getItemCount(): Int {
         return chatList.count()
     }
 
-    //메세지 어댑터를 완성하면 보내는 것, 받는 것 따로 작성해야함
-    //추상 클래스 사용하기로한다.
-//    inner class SendViewHolder(chatView: View) : ChatViewHolder<>(chatView) {
-//
-//
-//
-//    }
-//    inner class RecieveViewHolder(chatView: View) : ChatViewHolder<>(chatView) {
-//
-//
-//    }
-//    abstract class ChatViewHolder<T>(chatView: View): RecyclerView.ViewHolder(chatView){
-//        abstract fun set(itemView : T)
-//    }
+    //뷰홀더 추상 클래스
+    abstract class ChatViewHolder<ChatListItem>(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        abstract fun bind(item: ChatListItem)
+    }
+
+    //뷰홀더 추상 클래스 상속함 -> 받은 메세지 사용
+    inner class SendViewHolder(itemView: View) : ChatViewHolder<ChatListItem>(itemView) {
+        override fun bind(item: ChatListItem) {
+            val message = itemView.findViewById<TextView>(R.id.textView)
+            message.text = item.message
+        }
+    }
+    //뷰홀더 추상 클래스 상속함 -> 보내는 메세지 사용
+    inner class ReceiveViewHolder(itemView: View) : ChatViewHolder<ChatListItem>(itemView) {
+        override fun bind(item: ChatListItem) {
+            val message = itemView.findViewById<TextView>(R.id.textView)
+            message.text = item.message
+//            val time = itemView.findViewById<TextView>(R.id.tv_time)
+//            time.text = Util.getTime(item.createdAt)
+        }
+    }
+
 
 
 
--- Transaction_Project/app/src/main/java/com/example/transaction_project/chat/ChatListItem.kt
+++ Transaction_Project/app/src/main/java/com/example/transaction_project/chat/ChatListItem.kt
@@ -3,5 +3,6 @@ package com.example.transaction_project.chat
 data class ChatListItem(
     val userId : String,
     val sellerId : String,
+    val message : String
     //추가 할 사항??
-)
+){constructor():this("","","")}
