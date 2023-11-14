e8449e2a216607e1378147f58962ed28be5dfac3
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2023-11-14 22:48:08 +0900

    Changed getMessage, method Name
    Feat: changed getMessage
    
    두 가지 방식의 메세지 다시 불러오기 기능 구현 차후에 DB에 맞게 선택 예정

--- Transaction_Project/app/src/main/java/com/example/transaction_project/chat/ChatTestActivity.kt
+++ Transaction_Project/app/src/main/java/com/example/transaction_project/chat/ChatTestActivity.kt
@@ -3,23 +3,21 @@ package com.example.transaction_project.chat
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.transaction_project.R
-import com.example.transaction_project.home.ItemAdapter
-import com.example.transaction_project.home.Product
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import org.w3c.dom.Text
 
 
 class ChatTestActivity :AppCompatActivity() {
@@ -42,19 +40,18 @@ class ChatTestActivity :AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             finish()
         }
-        recyclerView = findViewById<RecyclerView>(R.id.message)
 
-
-
+        initResource()
         initItemAdapter()
-        initField()
-        setProfile()
-        getChat()
+        initSendMessage()
+        initProfile()
+        initMessage()
 
     }
 
-    //사용자 정보에 대한 값들을 저장하기 위한
-    private fun initField(){
+    //사용자 정보에 대한 값들을 저장하기 위하여 리소스 연결
+    private fun initResource(){
+        recyclerView = findViewById<RecyclerView>(R.id.message)
         item_img = findViewById<ImageView>(R.id.specific_img)
         item_title = findViewById<TextView>(R.id.specific_title)
         item_price = findViewById<TextView>(R.id.specific_price)
@@ -65,12 +62,37 @@ class ChatTestActivity :AppCompatActivity() {
         chatAdapter = ChatAdapter(chatList)
         recyclerView.adapter = chatAdapter
         recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
-        chatAdapter.notifyDataSetChanged()
+    }
+
+
+    private fun initSendMessage(){
+        val msgInput = findViewById<EditText>(R.id.wt_chat)
+        val sendMsg = findViewById<ImageButton>(R.id.sd_chat)
+
+        sendMsg.setOnClickListener {
+            if(msgInput.text.toString() != ""){
+                val tmp = ChatListItem("2","1",msgInput.text.toString(), Timestamp.now())
+                db.collection("/ChatList/userId/Chat").add(tmp)
+                    .addOnSuccessListener {
+                        msgInput.setText("")
+
+                        //새로운 메세지를 확인 하는 두 가지 방식 fs. chatlist에 추가하고 변경을 알려 초기화 sd. initMessage 다시 한 번 데이터 가져오기.
+                        chatList.add(tmp)  // 이 방식을 이용하면 버그가 발생 할 것 같음.
+                        chatAdapter.notifyDataSetChanged()
+                        //InitMessage()
+                    }
+                    .addOnFailureListener {
+                            exception ->
+                        Log.w("ChatTestActivity", "Error getting documents: $exception")
+                    }
+            }
+        }
     }
 
 
     //DB->CHATLIST->USERID->CHAT에 있는 컬렉션을 가져온다.
-    private fun getChat(){
+
+    private fun initMessage(){
             db.collection("/ChatList/userId/Chat")
                 .orderBy("timeAt",Query.Direction.ASCENDING).get() //timeAt을 기준으로 오름차순해서 메세지를 가져옵니다.
             .addOnSuccessListener { result->
@@ -89,7 +111,7 @@ class ChatTestActivity :AppCompatActivity() {
     }
 
     //물건의 정보를 세팅함. 가격 , 이름 , 이미지 , 사용자 이름
-    private fun setProfile(){
+    private fun initProfile(){
         db.document("/ChatList/userId").get()
             .addOnSuccessListener {
                 if(it["img"].toString() != ""){
@@ -109,6 +131,8 @@ class ChatTestActivity :AppCompatActivity() {
                 Log.w("ChatTestActivity", "Error getting documents: $exception")
             }
     }
+
+
 
 
 }
